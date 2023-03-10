ffb5b2c92796291c490bb10e98e9d5258ed5cc61
Sneha <snehalall.sneha@gmail.com>
2023-03-10 22:20:32 +0530

    fix:update .gitignore file

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,19 +1,20 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const redisClient = require('./redis');
+// const redisClient = require('./redis');
 
 const generateToken = async (id) => {
     let token = jwt.sign({ id: id }, process.env.JWT_SECRET, { expiresIn: '1d' });
 
-    await redisClient.set(token, '');
+    // await redisClient.set(token, '');
     return token;
 };
 const verifyToken = async (token) => {
     try {
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-        const tokenInRedis = await redisClient.get(token);
-        if (decodedToken && tokenInRedis != undefined) return decodedToken;
+        // const tokenInRedis = await redisClient.get(token);
+        // if (decodedToken && tokenInRedis != undefined) return decodedToken;
+        return decodedToken;
     } catch (error) {
         return false;
     }
